a730285840a693d4f2ba1d4b47c492a227a35db3
Ayush Singh <singhayush886@gmail.com>
2022-09-20 15:06:14 +0530

    Make Bottom Nav

---
+++ lib/screens/customer_home.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class CustomerHomeScreen extends StatefulWidget {
+  const CustomerHomeScreen({super.key});
+
+  @override
+  State<CustomerHomeScreen> createState() => _CustomerHomeScreenState();
+}
+
+class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
+  int _selectIndex = 0;
+  final List<Widget> _tabs = const [
+    Center(
+      child: Text("Home Screen"),
+    ),
+    Center(
+      child: Text("CategoryScreen"),
+    ),
+    Center(
+      child: Text("Stores Screen"),
+    ),
+    Center(
+      child: Text("Cart Screen"),
+    ),
+    Center(
+      child: Text("Profile Screen"),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _tabs[_selectIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        selectedFontSize: 20.4,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+        selectedItemColor: Colors.black,
+        currentIndex: _selectIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Color(0xffa0d2eb),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+            backgroundColor: Color(0xfff75990),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop),
+            label: 'Stores',
+            backgroundColor: Color(0xff7d3cff),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+            backgroundColor: Color(0xffe6dbc9),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+            backgroundColor: Color(0xff59ce8f),
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
